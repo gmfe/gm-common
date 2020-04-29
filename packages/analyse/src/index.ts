be0a574e99f646ae284f5ba9e0c70d392da6ff89
liyatang <liyatang@qq.com>
2020-04-29 17:56:40 +0800

    analyse

--- packages/analyse/src/index.ts
+++ packages/analyse/src/index.ts
@@ -4,9 +4,10 @@ import { report, getMetaData } from './util'
 // reportFirstScreen 上报首屏信息
 
 // 上报到企业微信的机器人，用户主动上报用。 否则太频繁。同时落到 /data/logs/fe/more/common.log
-function reportToQy(platform: string, data: any) {
+function reportToQy(platform: string, data?: any) {
   // try catch，可能 JSON.stringify 出错
   try {
+    data = data || {}
     data.metaData = Object.assign({}, data.metaData, getMetaData())
     // 简单文本即可
     report('https://trace.guanmai.cn/api/webhook/qy', {
@@ -24,4 +25,18 @@ function reportToQy(platform: string, data: any) {
   }
 }
 
-export { report, reportToQy }
+// 切换路由的时候可能拉取不到资源
+function reportRouteError(data: any) {
+  const url = 'https://trace.guanmai.cn/api/logs/more/' + __NAME__
+  report(url, data)
+}
+
+function reportPerformance(data?: any) {
+  const url = 'https://trace.guanmai.cn/api/logs/more/' + __NAME__
+  report(url, {
+    performanceTime: window.performance,
+    ...data,
+  })
+}
+
+export { report, reportToQy, reportPerformance, reportRouteError }
