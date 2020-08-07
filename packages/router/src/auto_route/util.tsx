f73c966817dd706d9aa2104f455f4309ddb76f41
liyatang <liyatang@qq.com>
2020-08-07 15:00:49 +0800

    package.json

---
+++ packages/router/src/auto_route/util.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react'
+import { LoadingComponentProps } from 'react-loadable'
+
+const Loading: FC<LoadingComponentProps> = (props) => {
+  const { retry, error } = props
+  if (error) {
+    return (
+      <div
+        style={{
+          textAlign: 'center',
+          padding: '20px',
+        }}
+      >
+        发生了错误！
+        <button onClick={retry}>重试</button>
+      </div>
+    )
+  } else {
+    return <div className='text-center gm-padding-20'>加载中...</div>
+  }
+}
+
+const NoMatch = () => {
+  return <div>404</div>
+}
+
+export { Loading, NoMatch }
