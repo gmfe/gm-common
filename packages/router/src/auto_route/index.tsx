cc6b9a0c25654ab755b9681bfd02d041b06c082c
liyatang <liyatang@qq.com>
2020-08-19 23:48:09 +0800

    fix: fixed AutoRouter

--- packages/router/src/auto_route/index.tsx
+++ packages/router/src/auto_route/index.tsx
@@ -14,24 +14,18 @@ interface RedirectBaseProps {
   from: string
 }
 
+interface NavConfigItem {
+  link: string
+  sub?: NavConfigItem[]
+}
+
+type NavConfig = NavConfigItem[]
+
 // src/pages index.page.xxx
 const req = require.context('@/pages', true, /index\.page\./, 'lazy')
 
 function getRouteList(Loading: any) {
-  const routeList: (RouteBaseProps | RedirectBaseProps)[] = []
-
-  // redirect from 去重，只需要一个即可
-  const fromMap: { [key: string]: any } = {}
-
-  function pushRedirect(what: RedirectBaseProps) {
-    const from = what.from as string
-
-    if (!fromMap[from]) {
-      routeList.push(what)
-
-      fromMap[from] = true
-    }
-  }
+  const routeList: RouteBaseProps[] = []
 
   _.each(req.keys(), (key) => {
     // ./home/index.page.xxx => /home
@@ -41,34 +35,6 @@ function getRouteList(Loading: any) {
     // 剩下就是路由部分
     const pathArr = key.slice(1).split('/').slice(0, -1)
 
-    // 业务最多三个层级，更深的是业务具体的页面
-    // 只有 2 3 层级需要 redirect
-    // /a        /a
-    // /a/b      /a->/a/b /a/b
-    // /a/b/c    /a->/a/b /a/b->/a/b/c /a/b/c
-    // /a/b/c/d  /a/b/c/d
-
-    if (pathArr.length === 3) {
-      // 一级跳二级
-      pushRedirect({
-        from: pathArr.slice(0, 2).join('/'),
-        to: pathArr.slice(0, 3).join('/'),
-      })
-    }
-
-    if (pathArr.length === 4) {
-      // 一级跳二级
-      pushRedirect({
-        from: pathArr.slice(0, 2).join('/'),
-        to: pathArr.slice(0, 3).join('/'),
-      })
-      // 二级跳三级
-      pushRedirect({
-        from: pathArr.slice(0, 3).join('/'),
-        to: pathArr.slice(0, 4).join('/'),
-      })
-    }
-
     routeList.push({
       path: pathArr.join('/'),
       loader: () => Promise.resolve(req(key)),
@@ -76,26 +42,13 @@ function getRouteList(Loading: any) {
   })
 
   const RouteList = _.map(routeList, (v) => {
-    const redirect = v as RedirectBaseProps
-    if (redirect.from) {
-      return (
-        <Redirect
-          key={redirect.from}
-          exact
-          from={redirect.from}
-          to={redirect.to}
-        />
-      )
-    }
-
-    const route = v as RouteBaseProps
     return (
       <Route
-        key={route.path}
+        key={v.path}
         exact
-        path={route.path}
+        path={v.path}
         component={Loadable({
-          loader: route.loader,
+          loader: v.loader,
           loading: Loading,
         })}
       />
@@ -105,17 +58,52 @@ function getRouteList(Loading: any) {
   return RouteList
 }
 
+function getRedirect(navConfig: NavConfig) {
+  const oneTwo: RedirectBaseProps[] = []
+  _.each(navConfig, (one) => {
+    if (one.sub?.length) {
+      oneTwo.push({
+        from: one.link,
+        to: one.sub[0].link,
+      })
+
+      _.each(one.sub, (two) => {
+        if (two.sub?.length) {
+          oneTwo.push({
+            from: two.link,
+            to: two.sub[0].link,
+          })
+        }
+      })
+    }
+  })
+
+  return _.map(oneTwo, (v) => {
+    return <Redirect key={v.from} exact from={v.from} to={v.to} />
+  })
+}
+
 export interface AutoRouterProps {
+  navConfig?: NavConfig
   NoMatch?: React.ComponentType<RouteComponentProps>
   Loading?: React.ComponentType<LoadingComponentProps>
   children?: React.ReactElement
 }
 
-const AutoRouter: FC<AutoRouterProps> = ({ NoMatch, Loading, children }) => {
+/**
+ * @pages/下三层目录
+ */
+const AutoRouter: FC<AutoRouterProps> = ({
+  navConfig,
+  NoMatch,
+  Loading,
+  children,
+}) => {
   // 优先用户的 children
   return (
     <Switch>
       {children}
+      {navConfig && getRedirect(navConfig)}
       {getRouteList(Loading)}
       <Route exact component={NoMatch} />
     </Switch>
