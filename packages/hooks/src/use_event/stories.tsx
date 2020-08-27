2142e3d14e25cbee3f998cf5dfad81c3f3110d81
tubulang <1053736438standard@gmail.com>
2020-08-27 11:57:44 +0800

    feat: useEvent、useKeyPress

---
+++ packages/hooks/src/use_event/stories.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useRef } from 'react'
+import useEvent from './index'
+
+export const Dom = () => {
+  const [height, setHeight] = useState(0)
+
+  const handleResize = () => {
+    console.log('height', height)
+    setHeight(window.innerHeight)
+  }
+  useEvent('resize', handleResize, { target: window })
+  return <div>监听window-resize事件,window.innerHeight: {height}</div>
+}
+
+export const RefDom = () => {
+  const [count, setCount] = useState(0)
+  const demoRef = useRef<HTMLDivElement>(null)
+  const handleClick = (event: any) => {
+    console.log(event, 'click')
+    setCount((count) => count + 1)
+  }
+  useEvent('click', handleClick, { target: demoRef })
+  return <div ref={demoRef}>监听click事件,点击次数: {count}</div>
+}
+
+export default {
+  title: 'Hooks/dom/useEvent',
+}
